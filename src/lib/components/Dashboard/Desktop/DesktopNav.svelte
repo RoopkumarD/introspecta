6fec2eac6a6c3ef4da586b77a490e1c0d5607ae0
RoopkumaD <dasroopkumar5@gmail.com>
2023-10-10 00:51:47 +0530

    doing tooltip than animation

--- src/lib/components/Dashboard/Desktop/DesktopNav.svelte
+++ src/lib/components/Dashboard/Desktop/DesktopNav.svelte
@@ -89,25 +89,25 @@
     </button>
 
     <div id="operations" class="flex items-center gap-4 mr-4">
-      <button on:click={goToHome} class="btn btn-sm group">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="lucide lucide-home"
-          ><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" /><polyline
-            points="9 22 9 12 15 12 15 22"
-          /></svg
-        >
-
-        <span class="hidden group-hover:block">Back Home</span>
-      </button>
+      <div class="tooltip tooltip-bottom font-bold" data-tip="Go Back Home">
+        <button on:click={goToHome} class="btn btn-square">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-home"
+            ><path
+              d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"
+            /><polyline points="9 22 9 12 15 12 15 22" /></svg
+          >
+        </button>
+      </div>
 
       <DeleteEntry />
       <SaveEntry encryptLog={workerApi.encryptLog} />
