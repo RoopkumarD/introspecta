3036207f96510c57848344316c40102d08ea48cc
RoopkumaD <dasroopkumar5@gmail.com>
2023-10-17 00:49:53 +0530

    csp headers

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,9 +13,33 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    csp: {
+      directives: {
+        "default-src": ["self"],
+        "script-src": [
+          "self",
+          "fonts.googleapis.com",
+          "https://accounts.google.com/gsi/client",
+          "wasm-unsafe-eval",
+          "https://apis.google.com",
+        ],
+        "font-src": ["self", "fonts.gstatic.com"],
+        "style-src": ["self", "fonts.googleapis.com"],
+        // connect-src to do xmlHttprequest
+        "connect-src": [
+          "self",
+          "https://accounts.google.com/gsi/",
+          "https://www.googleapis.com/upload/drive/v3/",
+          "https://www.googleapis.com/drive/v3/files/",
+        ],
+        "frame-src": [
+          "self",
+          "https://content.googleapis.com/",
+          "https://accounts.google.com/gsi/",
+        ],
+      },
+    },
   },
-
-  runtime: "nodejs18.x",
 };
 
 export default config;
